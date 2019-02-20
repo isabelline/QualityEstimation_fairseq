2fbcbe54485411f8a6fd472aa315ba1f0860ca77
isabelline <ehfalskfl1@snu.ac.kr>
2019-02-21 04:04:43 +0900

    Update utils.py

--- fairseq/utils.py
+++ fairseq/utils.py
@@ -18,7 +18,11 @@ from torch.serialization import default_restore_location
 
 def load_partial_weights(model, path):
     pretrained_dict = torch.load(path)
+    print("!!!!!!!!!")
+    print(pretrained_dict)
     model_dict = model.state_dict()
+    print("**************")
+    print(model_dict)
     pretrained_dict = {k:v for k,v in pretrained_dict.items() if k in model_dict}
     print("~~~~~~~~~~~~~~~~")
     print(pretrained_dict)
