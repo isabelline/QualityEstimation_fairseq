860f75a24f7767df62f6341409a2bd6cf28289e5
isabelline <ehfalskfl1@snu.ac.kr>
2019-02-20 19:36:23 +0900

    Update __init__.py

--- fairseq/tasks/__init__.py
+++ fairseq/tasks/__init__.py
@@ -73,7 +73,7 @@ for file in os.listdir(os.path.dirname(__file__)):
             TASK_REGISTRY[task_name].add_args(group_args)
             print("!!!!!!!!")
             for k in TASK_REGISTRY.keys():
-                print(k)
+                print(TASK_REGISTRY[k])
             globals()[task_name + '_parser'] = parser
 
 
